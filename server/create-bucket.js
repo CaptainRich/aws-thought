c8b249b5ee776386c15c76c9bb25ab3b3aaa0427
RichA <rich_ay@sbcglobal.net>
2021-05-15 14:31:53 -0500

    Improved commenting in 'create-bucket.js'.

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -1,21 +1,23 @@
-// Load the AWS SDK for Node.js
+// Load the AWS SDK for Node.js, which allows the application to communicate
+// with the web service.
 const AWS = require('aws-sdk');
 
-// create the S3 bucket name
+// Import the UUIDV4 package so we can create a unique S3 bucket name
 const { v4: uuidv4 } = require('uuid');
 
-// Configure the region
+// Configure the region (the region must be updated to communicate with the 
+// web service).
 AWS.config.update({region: 'us-east-2'});
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-// Create the parameters for calling createBucket
+// Create the object that assigns metadata for the bucket. 
 var bucketParams = {
     Bucket : "user-images-" + uuidv4()
   };
 
-  // call S3 to create the bucket
+  // call S3 to create the bucket, using the parameters just set.
 s3.createBucket(bucketParams, (err, data) => {
     if (err) {
       console.log("Error", err);
